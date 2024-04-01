9c574557df67ab46d85ff4d867e6afc15610798e
Suraj <hingadesuraj@gmail.com>
2024-04-01 21:18:59 +0530

    setup all middleware like cors policy, json etc

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,42 @@
 import express from "express";
 import { config } from "dotenv";
 import  connectToDb  from "./db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload"; // use for upload file from file manager
+import cloudinary from "cloudinary";
 
 const app = express();
 config({ path: "./config/config.env" }); // setup dotenv file 
+
+// middleware
+
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );// cors policy only access backend for frontend and dashboard url with request
+  
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/", 
+    })
+  );
+  
+  // cloudinary setup use documentation
+  cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 connectToDb();
+
 
 // console.log(process.env.PORT)
 
