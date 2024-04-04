a90b2a76771f77c9c0f67389a8cc26cdbc773078
Suraj <hingadesuraj@gmail.com>
2024-04-04 17:31:39 +0530

    update code in message controller.

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,28 +1,27 @@
+
 import { Message } from "../models/messageSchema.js";
 import {catchAsyncError} from '../middleware/catchAsyncError.js'
 import ErrorHandler from '../middleware/errorMiddleware.js'
 
+
 export const messageController = catchAsyncError(async (req, res, next) => {
   const { firstName, lastName, email, phone, message } = req.body;
 
-  try {
-    const messageData = await Message.create({
-      firstName,
-      lastName,
-      email,
-      phone,
-      message,
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Message Send Successfull..",
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(411).json({
-      success: false,
-      message: "some thing went wrong" + error.message,
-    });
+  if (!firstName || !lastName || !email || !phone || !message) {
+    // use error handler class here to pass error 
+    return next( new ErrorHandler("Please Fill Full Form!",400))
   }
+
+  await Message.create({ firstName, lastName, email, phone, message });
+
+  res.status(200).json({
+    success: true,
+    message: "Message send successfully!",
+  });
 });
+
+
+ 
+
+
+// flow of folder structure :::---->   model->controller(use modal and schema in controller file) ->router (use controller file in router folder) ->use in index.js
