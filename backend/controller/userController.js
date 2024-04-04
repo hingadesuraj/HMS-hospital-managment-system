438728854102ab462187472c46a2d1c25a9f8ffb
Suraj <hingadesuraj@gmail.com>
2024-04-04 17:57:12 +0530

    add new controller to add new admin using existing user and admin

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,72 +1,115 @@
 import { User } from "../models/userSchema.js";
-import {catchAsyncError} from '../middleware/catchAsyncError.js'
-import ErrorHandler from '../middleware/errorMiddleware.js'
+import { catchAsyncError } from "../middleware/catchAsyncError.js";
+import ErrorHandler from "../middleware/errorMiddleware.js";
 import { generateToken } from "../utils/jwtToken.js";
 
 // wrap all controller function in asyncerror handler
-export const patientRegister = catchAsyncError(async(req,res,next)=>{
-        const {firstName,lastName,email,phone,dob,gender,password,role,doctorDepartment,docAvatoar} = req.body;
+export const patientRegister = catchAsyncError(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    role,
+    doctorDepartment,
+    docAvatoar,
+  } = req.body;
 
-        if(!firstName || !lastName || !email || !phone  || !dob || !gender || !password || !role ){
-            return next(new ErrorHandler("Please Fill all fields ",400))
-        }
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !dob ||
+    !gender ||
+    !password ||
+    !role
+  ) {
+    return next(new ErrorHandler("Please Fill all fields ", 400));
+  }
 
-        const isExist = await User.findOne({email});
+  const isExist = await User.findOne({ email });
 
-        if(isExist){
-            return next(new ErrorHandler("User already exist, plese use other email address",400))
-        }
+  if (isExist) {
+    return next(
+      new ErrorHandler("User already exist, plese use other email address", 400)
+    );
+  }
 
-       await User.create({firstName,lastName,email,phone,dob,gender,password,role,doctorDepartment,docAvatoar})
+  await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    role,
+    doctorDepartment,
+    docAvatoar,
+  });
 
-       res.status(200).json({
-        success:true,
-        message :"Patient | User registration successfull"
-       })
-})
+  res.status(200).json({
+    success: true,
+    message: "Patient | User registration successfull",
+  });
+});
 
+// login
+export const loginPatient = catchAsyncError(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body;
 
-// login 
-export const loginPatient = catchAsyncError(async(req,res,next)=>{
-    const {email,password,confirmPassword,role} = req.body;
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new ErrorHandler("Please enter all fields", 400));
+  }
 
-    if(!email || !password || !confirmPassword || !role){
-        return next(new ErrorHandler("Please enter all fields",400))
-    }
+  if (password !== confirmPassword) {
+    return next(new ErrorHandler("Password does not match please check", 400));
+  }
 
-    if(password !== confirmPassword){
-        return next(new ErrorHandler("Password does not match please check",400))
-    }
+  const user = await User.findOne({ email }).select("+password");
+  // console.log(user)
+  if (!user) {
+    return next(new ErrorHandler("Invalid email id ", 400));
+  }
 
-    const user = await User.findOne({email}).select("+password");
-    // console.log(user)
-    if(!user){
-        return next(new ErrorHandler("Invalid email id ",400));
-    }
+  const isPasswordMatch = await user.comparePassword(confirmPassword);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Password does not match please check", 400));
+  }
 
-    const isPasswordMatch = await user.comparePassword(confirmPassword);
-    if(!isPasswordMatch){
-        return next(new ErrorHandler("Password does not match please check",400))
-    }
+  if (role !== user.role) {
+    return next(new ErrorHandler("User with this role not found ", 400));
+  }
 
-    if(role !== user.role){
-        return next(new ErrorHandler("User with this role not found ",400))
-    }
+  // const token = await user.generateJsonWebToken()
 
-    // const token = await user.generateJsonWebToken()
+  // console.log(token)
 
-    // console.log(token)
+  generateToken(user, "User Login Successfull.", 200, res);
 
-    generateToken(user,"User Login Successfull.",200,res)
-   
+  // res.status(200).json({
+  //     success:true,
+  //     message:"Login Successfull!",
+  //     token:token
+  // })
+});
 
-    // res.status(200).json({
-    //     success:true,
-    //     message:"Login Successfull!",
-    //     token:token
-    // })
-    
+// Add new Admin in database using old admin
 
-     
-
-})
+export const addNewAdmin = catchAsyncError(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    dob,
+    gender,
+    password,
+    doctorDepartment,
+    docAvatoar,
+  } = req.body;
+});
