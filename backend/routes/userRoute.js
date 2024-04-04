e40eec058a7fc2ec3222f9f6535fddca69bdda7d
Suraj <hingadesuraj@gmail.com>
2024-04-04 18:03:18 +0530

    add new route in user routes file

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { loginPatient, patientRegister } from '../controller/userController.js';
+import { addNewAdmin, loginPatient, patientRegister } from '../controller/userController.js';
 
 
 const router = express.Router();
@@ -8,7 +8,9 @@ const router = express.Router();
 
 // message public route 
 router.post("/signup",patientRegister)
-router.post("/login",loginPatient)
+router.post("/login",loginPatient);
+router.post("/admin/addnew",addNewAdmin);
+
 
 
 export default router
