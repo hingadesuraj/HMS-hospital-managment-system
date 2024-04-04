d1e81ec89211aaa8b612ce559c729b2e633f4fe1
Suraj <hingadesuraj@gmail.com>
2024-04-04 21:59:16 +0530

    get user details if auth by admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { addNewAdmin, getAllDoctor, loginPatient, patientRegister } from '../controller/userController.js';
+import { addNewAdmin, getAllDoctor, getUserDetails, loginPatient, patientRegister } from '../controller/userController.js';
 import { isAdminAuthenticated } from '../middleware/auth.js';
 
 
@@ -12,6 +12,7 @@ router.post("/signup",patientRegister)
 router.post("/login",loginPatient);
 router.post("/admin/addnew",isAdminAuthenticated,addNewAdmin);
 router.get("/doctors",getAllDoctor)
+router.get("/userdetails",isAdminAuthenticated,getUserDetails)
 
 
 
