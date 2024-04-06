c570309fe0e12552d2112271def32427e92d6a0e
Suraj <hingadesuraj@gmail.com>
2024-04-06 12:56:30 +0530

    changes in vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 // using module type then must be required full path include file extension like .js,.html
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import { catchAsyncError } from "./middleware/catchAsyncError.js";
+import ErrorHandler from "./middleware/errorMiddleware.js";
 
 // cloudinary setup use documentation
 cloudinary.v2.config({
@@ -9,12 +11,21 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-  // backend done
-});
-
+app.get(
+  "/",
+  catchAsyncError((req, res) => {
+    try {
+      res.status(200).send("Server is running...");
+      // backend done
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Internal Server error",
+      });
+      return next(new ErrorHandler("Internal Server error", 500));
+    }
+  })
+);
 
 // server listen port
 app.listen(process.env.PORT, () => {
